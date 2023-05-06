869ad338386d8e1e3862ed0352e94f342247313e
Cosimo Chellini <c.chellini@treedom.net>
2023-05-06 11:53:28 +0200

    feat(next-config): added proper config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    productionBrowserSourceMaps: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        unoptimized: true
+    },
+    generateEtags: false,
+}
 
 module.exports = nextConfig
