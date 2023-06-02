56e392057521e8b0d939442e5d0e12d475a1ebfa
Cosimo Chellini <c.chellini@treedom.net>
2023-06-02 15:32:51 +0200

    fix(test): added index page

---
+++ app/settings/page.tsx
@@ -0,0 +1,9 @@
+const SettingsPage = () => {
+  return (
+    <div className="flex flex-col w-full">
+      <h1 className={"card-title"}>Settings</h1>
+    </div>
+  );
+};
+
+export default SettingsPage;
