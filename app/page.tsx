250e63e088de333e882c3323ce85dfe6e46b9d0d
Cosimo Chellini <c.chellini@treedom.net>
2023-06-02 15:14:49 +0200

    feat(daisyui): added default ui

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -37,6 +38,11 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+      <div className='w-full'>
+        <button className={'btn'}>
+          daisy button
+        </button>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
